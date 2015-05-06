fd71a7cb7313dde8cbfba243594b66a2689e6d54
petermitrano <mitranopeter@gmail.com>
2015-05-06 14:35:40 -0400

    migrated to CrowdManipulationInterface

---
+++ AUTHORS.md
@@ -0,0 +1,9 @@
+Original Authors
+----------------
+
+ * [Russell Toris](http://users.wpi.edu/~rctoris/) (rctoris@wpi.edu)
+
+Contributors
+------------
+
+ * [Peter Mitrano](https://petermitrano.github.io) (robotwizard@wpi.edu)
---
+++ install.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Interface Setup Script
+#
+# Author: Russell Toris - rctoris@wpi.edu
+
+echo "  ____    _    ____  _     "
+echo " / ___|  / \  |  _ \| |    "
+echo "| |     / _ \ | |_) | |    "
+echo "| |___ / ___ \|  _ <| |___ "
+echo " \____/_/   \_\_| \_\_____|"
+echo
+
+echo
+echo "Carl Demo Interface Setup"
+echo "Author: Russell Toris - rctoris@wpi.edu"
+echo
+
+# check the directory we are working in
+DIR=`pwd`
+if [[ $DIR != *CrowdManipulationInterface ]]
+then
+	echo "ERROR: Please run this script in the 'CrowdManipulationInterface' directory."
+	exit;
+fi
+
+RMS="/var/www/rms"
+if [ ! -d "$RMS" ]; then
+	echo "ERROR: No RMS installation found in '$RMS'."
+	exit;
+fi
+
+echo "Copying 'app' scripts to local RMS directory..."
+cp app/Controller/*.php $RMS/Controller
+cp -r app/View/* $RMS/View
+cp -r app/webroot/img/* $RMS/webroot/img
+cp -r app/webroot/css/* $RMS/webroot/css
+
+echo "Installation complete!"
+echo
